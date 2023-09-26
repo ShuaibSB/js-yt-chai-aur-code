907b48cbbe07a1a867c0e484876b28894193396a
ShuaibSB <79365983+ShuaibSB@users.noreply.github.com>
2023-09-26 09:12:20 +0000

    value assigning

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("Hello World!")
+const myName = "Sam"; // we can't assign a value again
+let myEmail = "sam@google.com"; // This is a scope level variable {} 
+var state = "New Delhi"; // This is a global variable
+country = "India"; // This is a global variable
+
+console.table([myName,myEmail,state,country])
