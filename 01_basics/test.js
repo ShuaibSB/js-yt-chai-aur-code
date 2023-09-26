0152f1c50ba97d95916444c1514ccd9daafe74a5
ShuaibSB <79365983+ShuaibSB@users.noreply.github.com>
2023-09-26 09:56:10 +0000

    Conversion of data types

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,7 +1,51 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+
+
+        // Type of Variables
+/*
 const myName = "Sam"; // we can't assign a value again
 let myEmail = "sam@google.com"; // This is a scope level variable {} 
 var state = "New Delhi"; // This is a global variable
 country = "India"; // This is a global variable
 
-console.table([myName,myEmail,state,country])
+console.table([myName,myEmail,state,country]) 
+*/
+
+
+        // Data types
+/*
+"use strict"; // Treat all JS code as newer version
+alert(3+3) //It will not work because We are using nodejs, not browser
+
+Number => 2 to power 53
+bigint => for big numbers
+string => Value in quote ""
+boolean => true/false
+null => standalone value (typeof shows object)
+undefined => As name say not defined
+symbol => Use for uniqueness
+object
+
+console.log(typeof 1)
+console.log(typeof ("sam"))
+*/
+
+
+        // Conversion Operation
+
+/* let score = "A15"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score) // converting values in number
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+let someNumber = 15
+
+console.log(typeof someNumber)
+
+let valueInString = String(someNumber) // converting values in string
+console.log(typeof valueInString)
+console.log(valueInString)
+*/
